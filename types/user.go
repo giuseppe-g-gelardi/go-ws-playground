23e0bdf3df913e64fd8167dfb6f5de47bda766bd
giuseppe-g-gelardi <giuseppegelardi@icloud.com>
2023-10-12 22:16:57 -0400

    updated the User type to user bson instead of json and it magically works.

--- types/user.go
+++ types/user.go
@@ -1,12 +1,19 @@
-package types 
+package types
 
 import (
 	"go.mongodb.org/mongo-driver/bson/primitive"
 )
 
 type User struct {
-	Id        primitive.ObjectID `json:"_id"`
-	FirstName string    `json:"first_name"`
-	LastName  string    `json:"last_name"`
-	Email     string    `json:"email"`
+	Id        primitive.ObjectID `bson:"_id,omitempty"`
+	FirstName string             `bson:"first_name,omitempty"`
+	LastName  string             `bson:"last_name,omitempty"`
+	Email     string             `bson:"email,omitempty"`
 }
+
+// type User struct {
+// 	Id        primitive.ObjectID `json:"_id"`
+// 	FirstName string    `json:"first_name"`
+// 	LastName  string    `json:"last_name"`
+// 	Email     string    `json:"email"`
+// }
